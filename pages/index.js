c21a04f7d36505facc96402125851fb5965fc059
PavelSrom <srompavel98@gmail.com>
2020-05-11 14:49:54 +0200

    added MUI palette and styling

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Link from 'next/link'
+import { makeStyles } from '@material-ui/core/styles' // always this import
+
+const useStyles = makeStyles(theme => ({
+  // just to test
+  paragraph: {
+    color: theme.palette.secondary.main
+  }
+}))
 
 const Home = () => {
+  const classes = useStyles()
+
   return (
     <div>
-      <p>Homepage</p>
+      <p className={classes.paragraph}>Homepage</p>
 
       <Link href="/contact">
         <a>Go to contact page</a>
---
+++ utils/theme.js
@@ -0,0 +1,8 @@
+import { createMuiTheme } from '@material-ui/core'
+
+export default createMuiTheme({
+  palette: {
+    primary: { main: '#52969f' }, // I'm bluuue da ba dee ba da ba, da ba dee ba da ba
+    secondary: { main: '#f77e7f' } // sexy salmon color <3 #f77e7f (stolen) / #f58e80 / other
+  }
+})
