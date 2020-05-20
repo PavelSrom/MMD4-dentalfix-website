a6647770a63c0eaa184029ffc5eef89eb1a12cb7
PavelSrom <srompavel98@gmail.com>
2020-05-20 16:04:43 +0200

    font color

--- utils/theme.js
+++ utils/theme.js
@@ -3,6 +3,9 @@ import { createMuiTheme } from '@material-ui/core'
 export default createMuiTheme({
   palette: {
     primary: { main: '#52969f' }, // I'm bluuue da ba dee ba da ba, da ba dee ba da ba
-    secondary: { main: '#f77e7f' } // sexy salmon color <3 #f77e7f (stolen) / #f58e80 / other
+    secondary: { main: '#f77e7f' }, // sexy salmon color <3 #f77e7f (stolen) / #f58e80 / other
+    text: {
+      primary: '#3c3c3c'
+    }
   }
 })
