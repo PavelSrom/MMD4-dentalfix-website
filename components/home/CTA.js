5249f74598754b9a09e00a54fadafadb893ac0ef
Andreea Mihaela Ceachir <andreeaceachir@Andreeas-MacBook-Pro.local>
2020-05-24 14:39:34 +0200

    CTA

--- components/home/CTA.js
+++ components/home/CTA.js
@@ -2,9 +2,15 @@ import React from 'react'
 import { Container } from '../../templates/layout'
 import { ButtonLink } from '../../templates/button'
 import { Headline } from '../../templates/text'
+import chair from '../../assets/chair-2.jpg'
 
 const CTA = () => (
-  <Container background="#fff">
+  <div style={{minHeight: 300,
+    backgroundImage: `url(${chair})`,
+    backgroundSize: 'cover',
+    backgroundPosition: '50% 50%',
+    position: 'relative'}}>
+  <Container>
     <Headline centered style={{ marginBottom: 32 }}>
       Let's get started!
     </Headline>
@@ -18,6 +24,7 @@ const CTA = () => (
       Make an appointment
     </ButtonLink>
   </Container>
+  </div>
 )
 
 export default CTA
