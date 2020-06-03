0900be90a595d57151b431669d85d2967de6e567
PavelSrom <srompavel98@gmail.com>
2020-06-03 10:24:57 +0200

    color palette adjusted

--- utils/theme.js
+++ utils/theme.js
@@ -2,11 +2,9 @@ import { createMuiTheme } from '@material-ui/core/styles'
 
 export default createMuiTheme({
   palette: {
-    primary: { main: '#52969f' },
-    secondary: { main: '#f77e7f' },
-    text: {
-      primary: '#3c3c3c'
-    }
+    primary: { main: '#52969F' },
+    secondary: { main: '#E28079' },
+    text: { primary: '#3c3c3c' }
   },
   typography: {
     fontFamily: ['Raleway', 'sans-serif'].join(', ')
